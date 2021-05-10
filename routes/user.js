6b78558e66942534108788c4133fff2537569f14
Mohit-15 <programs.mohit15@gmail.com>
2021-05-11 01:30:09 +0530

    user update route added

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
-const { getUserById, getUser, getUsers } = require("../controllers/user");
+const { getUserById, getUser, getUsers, updateUser } = require("../controllers/user");
 
 router.param("id", getUserById);
-router.get("/user/:id",isSignedIn,getUser);
+router.get("/user/:id", isSignedIn, isAuthenticated, getUser);
 router.get("/users", getUsers);
+
+router.put("/user/:id", isSignedIn, updateUser);
+
 module.exports = router;
